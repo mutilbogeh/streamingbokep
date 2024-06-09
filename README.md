9ddb0ade4ee7861ee77a702d7bc692f0a41fd155
mutilbogeh <mutilbogeh@gmail.com>
2024-06-10 04:52:39 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# streamingbokep
-streamingbokep
+A live demo of the project is available at [https://streamingbokep.pages.dev](https://streamingbokep.pages.dev/).
+Fork the [repository](https://github.com/mutilbogeh/bocilsdcolmek) on GitHub.
---
+++ app/layout.tsx
@@ -0,0 +1,99 @@
+import "./globals.css";
+import Script from "next/script";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { SITENAME } from "@/lib/constants";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: `{SITENAME} - Video Bokep Indo Viral Terbaru`,
+    description: `{SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+    metadataBase: new URL("https://streamingbokep.pages.dev"),
+    alternates: {
+        canonical: `/`,
+    },
+    openGraph: {
+        title: `{SITENAME} - Video Bokep Indo Jepang Barat Viral Terbaru`,
+        description: `{SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        url: `/`,
+        type: `website`,
+    },
+    verification: {
+        google: 'qyQeDxg18BmEsvwd02zT5GSB8wtPpXblLcmINJfKOcU',
+        yandex: 'c333716f0457594d',
+    },
+};
+
+export const runtime = "edge";
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const jsonLd = {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": `{SITENAME}`,
+        "description": `{SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        "logo": "https://streamingbokep.pages.dev/favicon.ico",
+        "url": "https://streamingbokep.pages.dev",
+        "founder":{
+            "@type":"Person",
+            "name":"admin",
+            "url":"https://streamingbokep.pages.dev"
+            },
+            "foundingDate":"2024-06-10"
+        }
+        const jsonLd1 = {
+            "@context": "https://schema.org",
+        "@type": "WebPage",
+        "name": `{SITENAME}`,
+        "url": "https://streamingbokep.pages.dev",
+        "description": `{SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        "image": "https://streamingbokep.pages.dev/favicon.ico",
+        "potentialAction":{
+            "@type":"ReadAction",
+            "target":"https://streamingbokep.pages.dev"}
+        }
+        const jsonLd2 = {
+            "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": `{SITENAME}`,
+        "url": "https://streamingbokep.pages.dev",
+        "description": `{SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        "potentialAction": { 
+        "@type": "SearchAction", 
+          "target": "https://streamingbokep.pages.dev/?q={search_term}", 
+            "query-input": "required name=search_term"}
+        }
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={font.className}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd1) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd2) }}
+        />
+                    {children}
+                </ThemeProvider>
+            </body>
+<Script src="https://js.juicyads.com/jp.php?c=947403z2v256s2x2w2e4z2e4&u=http%3A%2F%2Fwww.juicyads.rocks"/>
+        </html>
+    );
+}
